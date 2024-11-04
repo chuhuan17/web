9aeec5626d148b06eaad3adbe01317055e94521a
chuhuan17 <huantx33@gmail.com>
2024-11-04 08:05:12 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,23 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="index.php">Thống kê doanh thu</a></li>
+                <li class="has-submenu">
+                    <a href="">Loại sản phẩm</a>
+                    <ul class="submenu">
+                        <li><a href="../loaisanpham/brandlist.php">Danh sách loại sản phẩm</a></li>
+                        <li><a href="../loaisanpham/brandadd.php">Thêm loại sản phẩm</a></li>
+                    </ul>
+                </li>
+                <li class="has-submenu">
+                    <a href="">Sản phẩm</a>
+                    <ul class="submenu">
+                        <li><a href="../sanpham/productlist.php">Danh sách sản phẩm</a></li>
+                        <li><a href="../sanpham/productadd.php">Thêm sản phẩm</a></li>
+                    </ul>
+                </li>
+                <li><a href="feedback.php">Phản hồi khách hàng</a></li>
+                <li><a href="setting.php">Cài đặt</a></li>
+                <li><a href="">Đăng xuất</a></li>
+            </ul>
+        </div>
